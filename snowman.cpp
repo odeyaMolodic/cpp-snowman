7a2ac69d4f43107ac12909dc672cc811d1dec5bc
odeyaMolodic <57504045+odeyaMolodic@users.noreply.github.com>
2021-03-17 12:14:07 +0200

    fix torso index and tidy warnings

--- snowman.cpp
+++ snowman.cpp
@@ -1,67 +1,90 @@
+/**
+ * DO TOU WANT TO BUILD A SNOWMAN?
+ * a program that takes in an eight digit number in the format HNLRXYTB
+ * where each letter is a digit from 1 to 4 that denotes which preset to use for the corresponding section of the snowman. 
+ * 
+ * Author: Odeya Lavi
+ * Since : 2021-03
+ */
+
 #include "snowman.hpp"
 #include <iostream>
+#include<array>
 using namespace std;
 
-const string H[] = {" _===_ ", "  ___  \n ..... ", "   _  \n  /_\\  ", "  ___  \n (_*_) "}; //for Hat
-const string N[] = {",", ".", "_", " "}; //for Nose
-const string LR[] = {".", "o", "O", "-"}; //for Left and Right Eye
-const string Xup[] = {" ", "\\", " ", " "}; //for Left Arm
-const string Yup[] = {" ", "/", " ", " "}; //for Right Arm
-const string Xdown[] = {"<", " ", "/", " "}; //for Left Arm
-const string Ydown[] = {">", " ", "\\", " "}; //for Right Arm
-const string T[] = {"( : )", "(] [)", "(> <)", "(   )"}; //for Torso
-const string B[] = {" ( : ) ", " (\" \") ", " (___) ", " (   ) "}; //for Base
+const array<string,4> H = {" _===_ ", "  ___  \n ..... ", "   _  \n  /_\\  ", "  ___  \n (_*_) "}; //for Hat
+const array<string,4> N = {",", ".", "_", " "}; //for Nose
+const array<string,4> LR = {".", "o", "O", "-"}; //for Left and Right Eye
+const array<string,4> Xup = {" ", "\\", " ", " "}; //for Left Arm
+const array<string,4> Yup = {" ", "/", " ", " "}; //for Right Arm
+const array<string,4> Xdown = {"<", " ", "/", " "}; //for Left Arm
+const array<string,4> Ydown = {">", " ", "\\", " "}; //for Right Arm
+const array<string,4> T = {"( : )", "(] [)", "(> <)", "(   )"}; //for Torso
+const array<string,4> B = {" ( : ) ", " (\" \") ", " (___) ", " (   ) "}; //for Base
 
-int n;
+const int MAX = 44444444;
+const int MIN = 11111111;
+const int SIZE = 8;
+const int DIGIT = 10;
 
 namespace ariel{
 
-    int getIndex(){
-        int index = n%10;
+    /*
+     * The function decomposes the input into digits
+     * get a reference to the input number
+     * Return the last digit (and deletes it).
+     */
+    int getIndex(int &ref){
+        int index = ref%DIGIT;
         if (index>4 || index<1) {
-            throw out_of_range("The number in each choice must be between 1 and 4");
+            throw out_of_range("Invalid input, the number in each choice must be between 1 and 4");
         }
-        n = n/10;
+        ref = ref/DIGIT;
         
         return index;
 
     }
 
-    /**
-     * a function that takes in an eight character string in the format HNLRXYTB
-     * where each letter is a digit from 1 to 4 that denotes which preset to use for the corresponding section of the snowperson. 
-     * Print the full snowperson to stdout.
-    */
+    /*
+     * Create and print the full snowman to stdout.
+     */
     string snowman(int num){
 
-        if (num<11111111 || num>44444444) {
-            throw out_of_range{"The number is not 8 digits long"};
+        if (num <= 0) {
+            throw out_of_range("Invalid input, the number must be positive");
         }
-        n = num;
 
-        string strSnowman = B[getIndex()-1] + "\n";
+        if (to_string(num).size() < SIZE || to_string(num).size() > SIZE) {
+            throw out_of_range("Invalid input, the number is not 8 digits long");
+        }
 
-        int torsoIndex = getIndex();
-        int rightArmIndex = getIndex();
-        strSnowman = Ydown[rightArmIndex-1] + "\n" + strSnowman;
+        if (num<MIN || num>MAX) {
+            throw out_of_range{"Invalid input, the number in each choice must be between 1 and 4"};
+        }
 
-        strSnowman = T[torsoIndex] + strSnowman;
+        string strSnowman = B.at(getIndex(num)-1) + "\n";
 
-        int leftArmIndex = getIndex();
-        strSnowman = Xdown[leftArmIndex-1] + strSnowman;
+        int torsoIndex = getIndex(num);
+        int rightArmIndex = getIndex(num);
+        strSnowman = Ydown.at(rightArmIndex-1) + "\n" + strSnowman;
 
-        strSnowman = Yup[rightArmIndex-1] + "\n" + strSnowman;
+        strSnowman = T.at(torsoIndex-1) + strSnowman;
 
-        strSnowman = LR[getIndex()-1] + ")" + strSnowman;
+        int leftArmIndex = getIndex(num);
+        strSnowman = Xdown.at(leftArmIndex-1) + strSnowman;
 
-        int leftEyeIndex = getIndex();
-        strSnowman = N[getIndex()-1] + strSnowman;
+        strSnowman = Yup.at(rightArmIndex-1) + "\n" + strSnowman;
 
-        strSnowman = "(" + LR[leftEyeIndex-1] + strSnowman;
+        strSnowman = LR.at(getIndex(num)-1) + ")" + strSnowman;
 
-        strSnowman = Xup[leftArmIndex-1] + strSnowman;
+        int leftEyeIndex = getIndex(num);
+        strSnowman = N.at(getIndex(num)-1) + strSnowman;
 
-        strSnowman = H[getIndex()-1] + "\n" + strSnowman;
+        strSnowman = "(" + LR.at(leftEyeIndex-1) + strSnowman;
+
+        strSnowman = Xup.at(leftArmIndex-1) + strSnowman;
+
+        strSnowman = H.at(getIndex(num)-1) + "\n" + strSnowman;
 
         return strSnowman;
 
